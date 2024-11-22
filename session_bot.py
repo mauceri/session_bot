1d48f3b46f844acadcea8291cd97fba6acff3f48
Christian Mauceri <cmauceri@gmail.com>
2024-11-22 21:41:46 +0100

    nettoyage

--- session_bot.py
+++ session_bot.py
@@ -2,7 +2,7 @@ import asyncio
 from session_bot_plugin_manager import PluginManager
 
 
-
+#boucle asyncio
 if __name__ == "__main__":
     pluginManager = PluginManager()
     asyncio.run(pluginManager.run())
